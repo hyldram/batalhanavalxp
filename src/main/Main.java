87cb11278dedc3b8023b2999cfd9ac763b11961b
fsplets <fsplets@localhost>
2011-09-18 21:30:36 -0300

    Adicionando Splash Screen

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,19 @@
 package main;
+
 import windows.*;
 
 public class Main {
 
 	public static void main(String args[]){
+		
+		// Para aumentar o tempo do Splash Screen
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		new BeginWindow();
 		//new BoardWindow();
 	}
